de6f036b5c28c7a62f1687cdc07045104e13f59d
Mohammed Irfan <kuttappuirfan244@gmail.com>
2025-02-25 03:17:14 +0530

    Irfan project status (#4)
    * feat: implement server monitoring and status reporting functionality
    
    * refactor: remove crypto-js dependency and add server configuration utility

---
+++ src/utils/serverConfig.ts
@@ -0,0 +1,3 @@
+export const serverConfig={
+    base_url : "http://localhost:3001"
+}
--- src/utils/serverReq.ts
+++ src/utils/serverReq.ts
@@ -1,10 +1,23 @@
-import axios from "axios";
+
+import axios from "axios"; 
 import { ServerStatusBody  } from "../types/types";
+import { serverConfig } from "./serverConfig";
+
 
 
 export const sendServerStatus = async (serverStatusBody: ServerStatusBody) => {
     try {
         await axios.post(
+            serverConfig.base_url + "/project/status",
+            serverStatusBody,
+            { headers: { "Content-Type": "application/json" } } 
+        );
+    } catch (error) {
+        console.log("Error sending server status:", error);
+    }
+};
+
+=======
             "http://localhost:3001/project/status",
             serverStatusBody
         );
@@ -14,4 +27,5 @@ export const sendServerStatus = async (serverStatusBody: ServerStatusBody) => {
         throw error;
     }
 }
+
 
